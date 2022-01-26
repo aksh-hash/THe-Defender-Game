0a2aa0d230b2d1fd2f80c05c7178c108f14fb96d
aksh-hash <74498340+aksh-hash@users.noreply.github.com>
2022-01-26 13:09:18 +0530

    Add files via upload

---
+++ src/MainMenu.java
@@ -0,0 +1,77 @@
+/**
+ * Code that specifies what is put into the menu. This also defines the frame that the game is played in.
+ */
+
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import java.awt.Toolkit;
+
+public class MainMenu extends JPanel implements ActionListener {
+
+    JPanel menuPanel = new JPanel();
+    JButton play = new JButton("Play");
+    private String menuscreen = "/Images/Title Screen.jpg";
+    ImageIcon MS = new ImageIcon(this.getClass().getResource(menuscreen));
+    private boolean visible = true;
+
+    public MainMenu() {
+        addKeyListener(new MainMenu.TAdapter());
+        setFocusable(true);
+        FlowLayout layout = new FlowLayout();
+        setLayout(layout);
+        setSize(800, 600);
+        JButton startButton = new JButton("Play");
+        play.setLocation (0, 0);
+        add(startButton);
+        //setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocation(100, 100);
+        setVisible(true);
+        
+        
+    }
+
+    public void paint(Graphics g) {
+        if (visible == true) {
+            super.paint(g);
+            // g.drawImage(MS.getImage(), 0, -20, this);
+        } else {
+            //System.out.println("GRATZ");
+            Toolkit.getDefaultToolkit().sync();
+            g.dispose();
+            //repaint ();
+        }
+    }
+
+    void play() {
+       // System.out.println("yolo");
+        add(new Background());
+    }
+
+    public void actionPerformed(ActionEvent event) {
+        String command = event.getActionCommand();
+        if (command.equals("Play")) {
+            play();
+        }
+        if (command.equals("Instructions")) {
+
+        }
+    }
+
+    private class TAdapter extends KeyAdapter {
+
+        public void keyPressed(KeyEvent e) {
+            visible = false;
+            repaint();
+            play();
+           // System.out.println("Yo");
+        }
+    }
+}
---
+++ src/Settings.java
@@ -0,0 +1,31 @@
+/**
+ *
+ * This is the main driver of the game.
+ */
+
+import javax.swing.JFrame;
+
+public class Settings extends JFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+
+    /*pre: none
+     post: sets the parameters for the Jframe. Starts the background class, and
+     with it the game*/
+    public Settings() {
+        add(new Background());
+        setTitle("The Defender");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setResizable(false);
+
+    }
+
+    public static void main(String[] args) {
+        new Settings();
+    }
+}
